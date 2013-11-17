259d34d2abe281af9719669e4958862e864e8e36
horzwxy <wangxiayang.fdt@gmail.com>
2013-11-17 22:26:13 +0800

    finish modify, need refactor

--- src/me/horzwxy/app/pfm/android/activity/LogInActivity.java
+++ src/me/horzwxy/app/pfm/android/activity/LogInActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,80 +26,53 @@ import me.horzwxy.app.pfm.model.data.User;
  */
 public class LogInActivity extends UnloggedInActivity {
 
-    private TextView accountNameTextView;
-    private TextView accountTypeTextView;
-    private Button submitButton;
     private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-        accountNameTextView = (TextView) findViewById(R.id.log_in_account_name);
-        accountTypeTextView = (TextView) findViewById(R.id.log_in_account_type);
-
-        Button chooseAccountButton = (Button) findViewById(R.id.log_in_choose_account);
-        submitButton = (Button) findViewById(R.id.log_in_submit);
-
-        chooseAccountButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AccountManager manager = AccountManager.get(LogInActivity.this);
-                final Account[] accounts = manager.getAccounts();
-                final String[] items = new String[accounts.length];
-                for (int i = 0; i < accounts.length; i++) {
-                    items[i] = parseAccountType(accounts[i].type) + ": " + accounts[i].name;
-                }
-                AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
-                builder.setTitle(R.string.log_in_choose_account)
-                        .setItems(items, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int which) {
-                                String name = accounts[which].name;
-                                accountNameTextView.setText(name);
-                                String accountType = parseAccountType(accounts[which].type);
-                                currentUser = new User(name, accountType);
-                                accountTypeTextView.setText(accountType);
-                                submitButton.setEnabled(true);
-                            }
-                        });
-                builder.create().show();
-            }
-        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        // get account record from SharedPreference
         final SharedPreferences sp = getPreferences(MODE_PRIVATE);
-        final String username = sp.getString( "username", null );
-        if( username != null ) {
-            AlertDialog.Builder builder = new AlertDialog.Builder( this );
-            builder.setTitle(getResources().getString(R.string.log_in_use_default_account))
-                    .setMessage(getResources().getString(R.string.log_in_use_default_account_hint1)
-                            + username + getResources().getString(R.string.log_in_use_default_account_hint2))
-                    .setCancelable(true)
-                    .setPositiveButton(getResources().getString(R.string.log_in_use_default_account_true),
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    currentUser = new User( username,
-                                            sp.getString( "nickname", null ),
-                                            sp.getString( "accountType", null ) );
-                                    onSubmit();
-                                }
-                            })
-                    .setNegativeButton(getResources().getString(R.string.log_in_use_default_account_false),
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    // nothing
-                                }
-                            } );
-            builder.create().show();
+        final String accountName = sp.getString( "accountName", null );
+        final String accountType = sp.getString( "accountType", null );
+        if( accountName != null && accountType != null ) {
+            User myAccount = getMatchedAccount( accountName, accountType );
+            if( myAccount != null ) {
+                myAccount.nickname = sp.getString( "nickname", null );
+                currentUser = myAccount;
+                // todo is this OK?
+                startActivity( new Intent( this, MainActivity.class ) );
+                return;
+            }
         }
+        // If info stored in SharedPreference matches none of the available accounts on device,
+        // clear the records in SharedPreference.
+        // This may happen when user logs in with one account then removes the account later.
+        sp.edit().putString( "accountName", null );
+        sp.edit().putString( "accountType", null );
+        sp.edit().putString( "nickname", null );
+        sp.edit().commit();
+    }
+
+    private User getMatchedAccount( String accountName, String accountType ) {
+        AccountManager manager = AccountManager.get(this);
+        final Account[] accounts = manager.getAccounts();
+
+        for ( Account account : accounts ) {
+            if( accountName.equals( account.name )
+                    && accountType.equals( account.type ) ) {
+                return new User( account.name, account.type );
+            }
+        }
+
+        return null;
     }
 
     private String parseAccountType(String accountTypeString) {
@@ -112,74 +84,124 @@ public class LogInActivity extends UnloggedInActivity {
         }
     }
 
+    public void onChooseAccount( View v ) {
+        AccountManager manager = AccountManager.get(this);
+        final Account[] accounts = manager.getAccounts();
+        final String[] items = new String[accounts.length];
+        for (int i = 0; i < accounts.length; i++) {
+            // build the result string
+            items[i] = parseAccountType(accounts[i].type) + ": " + accounts[i].name;
+        }
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.log_in_choose_account)
+                .setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String name = accounts[which].name;
+                        ( (TextView) findViewById(R.id.log_in_account_name) ).setText(name);
+                        String accountType = parseAccountType(accounts[which].type);
+                        ( (TextView) findViewById(R.id.log_in_account_type) ).setText(accountType);
+                        currentUser = new User(name, accountType);
+                        // enable the submit button
+                        findViewById(R.id.log_in_submit).setEnabled(true);
+                    }
+                });
+        builder.create().show();
+    }
+
     public void onSubmit( View v ) {
         onSubmit();
     }
 
     private void onSubmit() {
         pDialog = new ProgressDialog(LogInActivity.this);
+        final LoggingInTask task = new LoggingInTask();
         pDialog.setCancelable(true);
+        pDialog.setOnCancelListener( new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                task.cancel( true );
+            }
+        });
         pDialog.setMessage(getResources().getString(R.string.logging_in));
         pDialog.show();
-        LogInRequest request = new LogInRequest(currentUser);
-        new LoggingInTask().execute(request);
+        task.execute(new LogInRequest(currentUser));
     }
 
-    private void onLogInSucceed( String username, String nickname, String accountType ) {
+    private void onAccountVerified( String nickname ) {
         SharedPreferences.Editor spEditor = getPreferences( MODE_PRIVATE ).edit();
-        spEditor.putString( "username", username );
-        spEditor.putString( "nickname", nickname );
-        spEditor.putString( "accountType", accountType );
+        spEditor.putString("accountName", currentUser.accountName);
+        spEditor.putString("nickname", nickname);
+        spEditor.putString("accountType", currentUser.accountType);
         spEditor.commit();
-        currentUser = new U
+        // currentUser has been instantialized before.
+        // only need to set nickname
+        currentUser.nickname = nickname;
+
+        startActivity( new Intent(this, MainActivity.class) );
+        this.finish();
+    }
+
+    private void onAccountRegisted() {
+        onAccountVerified( currentUser.nickname );
+    }
+
+    private void showSetNicknameDialog( int messageStringId ) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.set_nickname);
+        builder.setMessage(messageStringId);
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(LogInActivity.this);
+        builder.setView(input)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        currentUser.nickname = input.getEditableText().toString();
+                        final SetNicknameTask task = new SetNicknameTask();
+                        pDialog.dismiss();
+                        pDialog = new ProgressDialog(LogInActivity.this);
+                        pDialog.setCancelable(true);
+                        pDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialogInterface) {
+                                onCancleSetNickname();
+                            }
+                        });
+                        pDialog.setMessage(getResources().getString(R.string.setting_nickname_hint));
+                        pDialog.show();
+                        task.execute(new SetNicknameRequest(currentUser));
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        onCancleSetNickname();
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void onCancleSetNickname() {
+        Toast.makeText(LogInActivity.this, R.string.cannot_no_nickname_hint, Toast.LENGTH_SHORT).show();
     }
 
     class LoggingInTask extends PFMHttpAsyncTask<LogInRequest, LogInResponse> {
 
         @Override
         protected LogInResponse doInBackground(LogInRequest... requests) {
-            String responseString = doConnecting( requests[0] );
+            String responseString = doConnecting(requests[0]);
             return Response.parseResponse( responseString, LogInResponse.class );
         }
 
         @Override
         protected void onPostExecute(LogInResponse response) {
+            pDialog.dismiss();
             if (response.type == LogInResponse.ResultType.SUCCEED) {
-                pDialog.dismiss();
-                currentUser.nickname = response.nickname;
-                Intent intent = new Intent(LogInActivity.this, MainActivity.class);
-                startActivity(intent);
-                LogInActivity.this.finish();
+                onAccountVerified(response.nickname);
             } else if (response.type == LogInResponse.ResultType.NEED_REGISTER) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(LogInActivity.this);
-                alert.setTitle(R.string.set_nickname);
-                alert.setMessage(R.string.set_nickname_hint);
-
-                // Set an EditText view to get user input
-                final EditText input = new EditText(LogInActivity.this);
-                alert.setView(input);
-                alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String nickname = input.getEditableText().toString();
-                        currentUser.nickname = nickname;
-                        SetNicknameRequest request = new SetNicknameRequest(currentUser);
-                        new SetNicknameTask().execute(request);
-                        pDialog.dismiss();
-                        pDialog = new ProgressDialog(LogInActivity.this);
-                        pDialog.setCancelable(true);
-                        pDialog.setMessage(getResources().getString(R.string.setting_nickname_hint));
-                        pDialog.show();
-                    }
-                });
-                alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        Toast.makeText(LogInActivity.this, R.string.cannot_no_nickname_hint, Toast.LENGTH_SHORT).show();
-                    }
-                });
-                alert.show();
-                pDialog.dismiss();
+                showSetNicknameDialog( R.string.set_nickname_hint );
             } else {
-                pDialog.dismiss();
                 Toast.makeText(LogInActivity.this, R.string.fail_log_in, Toast.LENGTH_SHORT).show();
             }
         }
@@ -195,42 +217,12 @@ public class LogInActivity extends UnloggedInActivity {
 
         @Override
         protected void onPostExecute(SetNicknameResponse response) {
+            pDialog.dismiss();
             if (response.type == SetNicknameResponse.ResultType.SUCCESS) {
-                pDialog.dismiss();
-                Intent intent = new Intent(LogInActivity.this, MainActivity.class);
-                startActivity(intent);
-                LogInActivity.this.finish();
-                return;
+                onAccountRegisted();
             } else if (response.type == SetNicknameResponse.ResultType.USED) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(LogInActivity.this);
-                alert.setTitle(R.string.set_nickname);
-                alert.setMessage(R.string.setting_nickname_used);
-
-                // Set an EditText view to get user input
-                final EditText input = new EditText(LogInActivity.this);
-                alert.setView(input);
-                alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String nickname = input.getEditableText().toString();
-                        currentUser.nickname = nickname;
-                        SetNicknameRequest request = new SetNicknameRequest(currentUser);
-                        new SetNicknameTask().execute(request);
-                        pDialog.dismiss();
-                        pDialog = new ProgressDialog(LogInActivity.this);
-                        pDialog.setCancelable(true);
-                        pDialog.setMessage(getResources().getString(R.string.setting_nickname_hint));
-                        pDialog.show();
-                    }
-                });
-                alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        Toast.makeText(LogInActivity.this, R.string.cannot_no_nickname_hint, Toast.LENGTH_SHORT).show();
-                    }
-                });
-                alert.show();
-                pDialog.dismiss();
+                showSetNicknameDialog( R.string.setting_nickname_used );
             } else {
-                pDialog.dismiss();
                 Toast.makeText(LogInActivity.this, R.string.setting_nickname_failed, Toast.LENGTH_SHORT).show();
             }
         }
